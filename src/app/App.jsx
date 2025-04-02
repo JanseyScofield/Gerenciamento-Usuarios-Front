0c2c8ad0f72d93dfc01b15c35d051071575346e8
JanseyScofield <janscofield10@gmail.com>
2025-04-02 15:22:36 -0300

    Adicionando funcionalidade de pesquisar um usuário por nome

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -5,25 +5,61 @@ import { useState, useEffect } from "react";
 export const App = () => {
     const urlApi = "https://localhost:7100/usuarios";
     const [usuarios, setUsuarios] = useState([]);
+    const [nomePesquisado, setNomePesquisado] = useState("");
 
-    useEffect(() => {
+    const receberTodosUsuarios = () =>{
         axios.get(urlApi)
-        .then((response) => {
-            console.log(response.data)
-            setUsuarios(response.data.listaUsuarios)
-        })
-        .catch((error) => {
-            console.error(error);
-        })   
-    }, []);
+            .then((response) => {
+                setUsuarios(response.data.listaUsuarios);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
 
-    return(
+    useEffect(receberTodosUsuarios, []);
+
+    const pesquisarUsuarioNome = () => {
+        if(!nomePesquisado){
+            receberTodosUsuarios();
+        }
+        else{
+            axios.get(urlApi + "/" + nomePesquisado)
+            .then((response) => {
+                setUsuarios([response.data]);
+            })  
+            .catch((error) =>{
+                const mensagem = "Usuário não encontrado";
+
+                const usuarioNaoEncontrado = {
+                    id : -1,
+                    nome : mensagem,
+                    email : "",
+                    idade : ""
+                };
+
+                setUsuarios([usuarioNaoEncontrado]);
+            });
+        }
+    }
+
+    return (
         <main className="container">
             <h1 className="text-center">Gerenciamento de Usuários</h1>
-            <section className="border rounded p-5 mt-4 shadow-lg" style={{ maxHeight: '500px', overflowY: 'auto' }}>
-            {usuarios.map((usuario) => {
-                return <InfoUsuario key={usuario.id} nome={usuario.nome} email={usuario.email} idade={usuario.idade}/>
-            })}
+            <div className="d-flex" style={{ marginBottom: "-1rem", marginTop: "2rem" }}>
+                <input 
+                    className="border rounded shadow-sm w-50 form-control-lg mt2" 
+                    type="text" 
+                    value={nomePesquisado} 
+                    placeholder="Digite um nome"
+                    onChange={(e) => setNomePesquisado(e.target.value)}
+                />
+                <button className="btn btn-lg border h-100" onClick={pesquisarUsuarioNome}><i className="bi bi-search"></i></button>
+            </div>
+            <section className="border rounded p-5 mt-4 shadow-lg" style={{ maxHeight: '700px', overflowY: 'auto' }}>
+                {usuarios.map((usuario) => {
+                    return <InfoUsuario key={usuario.id} nome={usuario.nome} email={usuario.email} idade={usuario.idade} />
+                })}
             </section>
         </main>
     );
