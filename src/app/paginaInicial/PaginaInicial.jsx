3728f9cec14adccd7c2f25aee31e176ccf0f5d29
JanseyScofield <janscofield10@gmail.com>
2025-04-02 22:48:56 -0300

    Refatorando a arquitetura do projeto separando o componente de PaginaInicial do App

---
+++ src/app/paginaInicial/PaginaInicial.jsx
@@ -0,0 +1,66 @@
+import { InfoUsuario } from "../infoUsuario/InfoUsuario.jsx";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+export const PaginaInicial = () => {
+    const urlApi = "https://localhost:7100/usuarios";
+    const [usuarios, setUsuarios] = useState([]);
+    const [nomePesquisado, setNomePesquisado] = useState("");
+
+    const receberTodosUsuarios = () =>{
+        axios.get(urlApi)
+            .then((response) => {
+                setUsuarios(response.data.listaUsuarios);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+
+    useEffect(receberTodosUsuarios, []);
+
+    const pesquisarUsuarioNome = () => {
+        if(!nomePesquisado){
+            receberTodosUsuarios();
+        }
+        else{
+            axios.get(urlApi + "/" + nomePesquisado)
+            .then((response) => {
+                setUsuarios([response.data]);
+            })  
+            .catch((error) =>{
+                const mensagem = "Usuário não encontrado";
+
+                const usuarioNaoEncontrado = {
+                    id : -1,
+                    nome : mensagem,
+                    email : "",
+                    idade : ""
+                };
+
+                setUsuarios([usuarioNaoEncontrado]);
+            });
+        }
+    }
+
+    return (
+        <section className="container">
+            <h1 className="text-center">Gerenciamento de Usuários</h1>
+            <div className="d-flex" style={{ marginBottom: "-1rem", marginTop: "2rem" }}>
+                <input 
+                    className="border rounded shadow-sm w-50 form-control-lg mt2" 
+                    type="text" 
+                    value={nomePesquisado} 
+                    placeholder="Digite um nome"
+                    onChange={(e) => setNomePesquisado(e.target.value)}
+                />
+                <button className="btn btn-lg border h-100" onClick={pesquisarUsuarioNome}><i className="bi bi-search"></i></button>
+            </div>
+            <section className="border rounded p-5 mt-4 shadow-lg" style={{ maxHeight: '700px', overflowY: 'auto' }}>
+                {usuarios.map((usuario) => {
+                    return <InfoUsuario key={usuario.id} nome={usuario.nome} email={usuario.email} idade={usuario.idade} />
+                })}
+            </section>
+        </section>
+    );
+}
